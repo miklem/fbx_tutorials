38b05da3450f1aa897c7ef3f5cc8738a751cb0fc
Mikhail M <mmiroshnichenko@croc.ru>
2018-12-05 14:13:20 +0300

    transformations check
    + get offset
    + print wrong mesh

--- FBX_getUV.py
+++ FBX_getUV.py
@@ -23,11 +23,30 @@ print child.GetName()  # chest
 mesh = child.GetNodeAttribute()
 
 polyCount = mesh.GetPolygonCount()
-print polyCount  # 6
+
 layerCount = mesh.GetLayerCount()
 print layerCount
 
-# UV
+
+def checkTranslation(matrix=None, node=None):
+    if matrix and node:
+        if any(val != 0 for val in matrix):
+            print " found offset %s at: %s" % (matrix, node.GetName())
+
+
+# def checkUV01(node=None):
+#     if node:
+#         checkUVs = [j for i in uv_values for j in i if (j > 1.0 or j < 0.0)]
+#         if len(checkUVs) > 0:
+#             print checkUVs
+
+
+def get_all_nodes(node):
+    nodes.append(node)
+    for i in range(0, node.GetChildCount()):
+        get_all_nodes(node.GetChild(i))
+
+
 for z in range(layerCount):
     mesh_uvs = mesh.GetLayer(z).GetUVs()
     if not mesh_uvs:
@@ -44,36 +63,56 @@ for z in range(layerCount):
         uv = uvs_array.GetAt(l)
         uv = [uv[0], uv[1]]
         uv_values.append(uv)
-    print uv_values
+    # print uv_values
 
-    #classical for cycle
-    checkUVs = []
-    for i in uv_values:
-        for j in i:
-            if j > 1.0 or j < 0.0:
-                checkUVs.append(j)
+    # #classical for cycle
+    # checkUVs = []
+    # for i in uv_values:
+    #     for j in i:
+    #         if j > 1.0 or j < 0.0:
+    #             checkUVs.append(j)
     #
-    print checkUVs
-    #same result
+    # print checkUVs
+
     checkUVs = [j for i in uv_values for j in i if (j > 1.0 or j < 0.0)]
     print checkUVs
+    if len(checkUVs) > 0:
+        print checkUVs
 
-nodes = []
 
 
-
-def get_all_nodes(node, currentpath=[]):
-    currentpath.append(node.GetName())
-    nodes.append(list(currentpath))
-
-    print "Path: %s" % currentpath
-    for i in range(0, node.GetChildCount()):
-
-        get_all_nodes(node.GetChild(i), currentpath)
-    currentpath.pop()
+nodes = []  # type: List[fbx.FbxNode]
 
 
 get_all_nodes(root)
 print nodes
+
+
+
+
+#
+#transformation
+#
+for node in nodes:  # type: fbx.FbxNode
+    mesh = node.GetNodeAttribute()
+    geometry = fbx.FbxGeometryConverter(manager)
+    mesh = geometry.Triangulate(mesh, True)
+    # print attribute
+    # print node.GetNodeAttribute()
+    lTMPVector = node.GetGeometricTranslation(fbx.FbxNode.eSourcePivot)  # type: fbx.FbxVector4
+    position = [lTMPVector[0], lTMPVector[1], lTMPVector[2]]
+    # print position
+    localMatrix = node.EvaluateLocalTranslation()  # type: fbx.FbxVector4
+    localMatrix = [localMatrix[0], localMatrix[1], localMatrix[2]]
+    checkTranslation(localMatrix, node)
+
+
+
+
+
+
+
+
+
 
 
